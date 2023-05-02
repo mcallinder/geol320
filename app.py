6ad50d34d2b52de66f16e7b7ee8d1d77a012893f
Mark <mcallinder@users.noreply.github.com>
2023-05-02 12:15:07 -0700

    Changed to UH data.

--- app.py
+++ app.py
@@ -2,26 +2,39 @@ from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+# San Francisco Daily https://uhslc.soest.hawaii.edu/data/
+data = "https://uhslc.soest.hawaii.edu/data/csv/rqds/pacific/daily/d551a.csv"
+headers = ["year", "month", "day", "level"]
+df = pd.read_csv(data, names=headers, na_values=-32767)
+
+years = df.year.unique()
 
 app = Dash(__name__)
 
 server = app.server
 
 app.layout = html.Div([
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
-    dcc.Graph(id='graph-content')
+    html.H1(children='GEOL 320 App', style={'textAlign': 'center'}),
+    dcc.Slider(
+        min=years[0],
+        max=years[-1],
+        step=1,
+        value=years[0],
+        id='years',
+        marks=None,
+        tooltip={"placement": "bottom", "always_visible": True},
+    ),
+    dcc.Graph(id='graph')
 ])
 
 
 @callback(
-    Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
+    Output('graph', 'figure'),
+    Input('years', 'value')
 )
 def update_graph(value):
-    dff = df[df.country==value]
-    return px.line(dff, x='year', y='pop')
+    dff = df[df.year == value]
+    return px.scatter(dff, x='day', y='level')
 
 
 if __name__ == '__main__':
