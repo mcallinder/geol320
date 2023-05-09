eac246ecb43c498884f170d71b7459f79d2c3949
Mark <mcallinder@users.noreply.github.com>
2023-05-09 07:59:47 -0700

    Initial app, added data.py, added local data files.

--- app.py
+++ app.py
@@ -1,41 +1,233 @@
-from dash import Dash, html, dcc, callback, Output, Input
+import data
+from dash import Dash, html, dcc, Output, Input
+import dash_bootstrap_components as dbc
+import plotly.graph_objects as go
 import plotly.express as px
 import pandas as pd
+from dash.exceptions import PreventUpdate
 
-# San Francisco Daily https://uhslc.soest.hawaii.edu/data/
-data = "https://uhslc.soest.hawaii.edu/data/csv/rqds/pacific/daily/d551a.csv"
+# Header styles
+h1_style = {'textAlign': 'center'}
+h2_style = {'textAlign': 'left'}
+
+# CO2 Concentration data and figure
+co2_df1 = pd.read_csv(data.co2_data1, skiprows=61, nrows=781, usecols=[3, 8])
+co2_df2 = pd.read_csv(data.co2_data2, skiprows=55, usecols=[2, 3])
+co2_df1.columns = ["Date", "CO2"]
+co2_df2.columns = ["Date", "AVG"]
+co2_fig = go.Figure()
+co2_fig.add_trace(go.Scatter(x=co2_df1['Date'], y=co2_df1['CO2'], mode='lines', name='Mauna Loa CO2 Record'))
+co2_fig.add_trace(go.Scatter(x=co2_df2['Date'], y=co2_df2['AVG'], mode='lines', name='Globally averaged marine surface'))
+co2_fig.update_layout(title='Monthly CO2 Concentration', xaxis_title='Date', yaxis_title='CO2 in ppm')
+
 headers = ["year", "month", "day", "level"]
-df = pd.read_csv(data, names=headers, na_values=-32767)
+uhslc_dfs = []
+for uhslc_station in data.uhslc_list:
+    uhslc_df = pd.read_csv(uhslc_station[2], names=headers, na_values=-32767)
+    uhslc_df['location'] = uhslc_station[1]
+    uhslc_df['latitude'] = uhslc_station[3][0]
+    uhslc_df['longitude'] = uhslc_station[3][1]
+    uhslc_dfs.append(uhslc_df)
+uhslc_df = pd.concat(uhslc_dfs, ignore_index=True)
+uhslc_df['date'] = pd.to_datetime(uhslc_df[['year', 'month', 'day']])
 
-years = df.year.unique()
+psmsl_dfs = []
+for psmsl_station in data.psmsl_list:
+    psmsl_df = pd.read_csv(psmsl_station[2], sep=';', usecols=[0, 1], names=['year', 'level'], na_values=-99999)
+    psmsl_df['location'] = psmsl_station[1]
+    psmsl_df['latitude'] = psmsl_station[4][0]
+    psmsl_df['longitude'] = psmsl_station[4][1]
+    psmsl_dfs.append(psmsl_df)
+psmsl_df = pd.concat(psmsl_dfs, ignore_index=True)
 
-app = Dash(__name__)
+psmsl_dfs_an = []
+for psmsl_station in data.psmsl_list:
+    psmsl_df_an = pd.read_csv(psmsl_station[3], sep=';', usecols=[0, 1], names=['year', 'level'], na_values=-99999)
+    psmsl_df_an['location'] = psmsl_station[1]
+    psmsl_df_an['latitude'] = psmsl_station[4][0]
+    psmsl_df_an['longitude'] = psmsl_station[4][1]
+    psmsl_dfs_an.append(psmsl_df_an)
+psmsl_df_an = pd.concat(psmsl_dfs_an, ignore_index=True)
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 server = app.server
 
 app.layout = html.Div([
-    html.H1(children='GEOL 320 App', style={'textAlign': 'center'}),
-    dcc.Slider(
-        min=years[0],
-        max=years[-1],
-        step=1,
-        value=years[0],
-        id='years',
-        marks=None,
-        tooltip={"placement": "bottom", "always_visible": True},
-    ),
-    dcc.Graph(id='graph')
+    html.H1('GEOL 320 App', style=h1_style),
+    html.Br(),
+    dbc.Row([
+        dbc.Col(md=0, lg=1),
+        dbc.Col([
+            dbc.Tabs([
+                dbc.Tab([
+                    html.Br(),
+                    html.H2('Tide Gauge', style=h2_style),
+                    html.Br(),
+                    dbc.Row([
+                        dbc.Col(md=1, lg=1),
+                        dbc.Col([
+                            dcc.Dropdown(id='station', multi=True, options=[{'label': location, 'value': location} for location in psmsl_df['location'].unique()],value=['San Francisco']),
+                        ], lg=6),
+                        dbc.Col([
+                            dcc.Slider(id='year_slider', value=1980, min=1860, max=2022, step=5, marks=None, tooltip={"placement": "bottom", "always_visible": True})
+                        ], lg=4)
+                    ]),
+                    html.Br(),
+                    dbc.Row([
+                        dbc.Col(md=1, lg=1),
+                        dbc.Col([
+                            dcc.RadioItems([
+                                {'label': html.Div(['Monthly'],
+                                                   style={'margin-right': '20px', 'display': 'inline-block'}),
+                                 'value': True},
+
+                                {'label': html.Div(['Annually'],
+                                                   style={'margin-right': '20px', 'display': 'inline-block'}),
+                                 'value': False}
+
+                            ], id='is_monthly', value=True, inline=True, style={}),
+                        ], lg=3),
+                        dbc.Col([
+                            dcc.RadioItems([
+                                {'label': html.Div(['Line'],
+                                                   style={'margin-right': '20px', 'display': 'inline-block'}),
+                                 'value': 'lines'},
+
+                                {'label': html.Div(['Scatter'],
+                                                   style={'margin-right': '20px', 'display': 'inline-block'}),
+                                 'value': 'markers'}
+
+                            ], id='mode', value='lines', inline=True, style={}),
+                        ], lg=2),
+                        dbc.Col([
+                            dcc.RadioItems([
+                                {'label': html.Div(['OLS'],
+                                                   style={'margin-right': '20px', 'display': 'inline-block'}),
+                                 'value': 'ols'},
+
+                                {'label': html.Div(['Lowess'],
+                                                   style={'margin-right': '20px', 'display': 'inline-block'}),
+                                 'value': 'lowess'}
+
+                            ], id='trendline', value='ols', inline=True, style={}),
+                        ], lg=2),
+                        dbc.Col([
+                            dcc.RadioItems([
+                                {'label': html.Div(['Trace'],
+                                                   style={'margin-right': '20px', 'display': 'inline-block'}),
+                                 'value': 'trace'},
+
+                                {'label': html.Div(['Overall'],
+                                                   style={'margin-right': '20px', 'display': 'inline-block'}),
+                                 'value': 'overall'}
+
+                            ], id='scope', value='trace', inline=True, style={}),
+                        ], lg=2)
+                    ]),
+                    html.Br(),
+                    dbc.Row([
+                        dbc.Col([
+                            dcc.Graph(id='location')
+                        ], lg=6),
+                        dbc.Col([
+                            dcc.Graph(id='psmsl')
+                        ], lg=6)
+                    ])
+                ], label='Tide Gauge'),
+                dbc.Tab([
+                    html.Br(),
+                    html.H2('CO2 Concentration', style=h2_style),
+                    html.Br(),
+                    dcc.Graph(figure=co2_fig)
+                ], label='CO2 Concentration'), dbc.Tab([
+                    html.Br(),
+                    html.H2('About', style=h2_style)
+                ], label='About')
+            ])
+        ], md=12, lg=10)
+    ])
 ])
 
 
-@callback(
-    Output('graph', 'figure'),
-    Input('years', 'value')
+@app.callback(
+    Output('psmsl', 'figure'),
+    Input('station', 'value'),
+    Input('year_slider', 'value'),
+    Input('is_monthly', 'value'),
+    Input('mode', 'value'),
+    Input('trendline', 'value'),
+    Input('scope', 'value')
 )
-def update_graph(value):
-    dff = df[df.year == value]
-    return px.scatter(dff, x='day', y='level')
+def psmsl(locations, year, is_monthly, mode, trendline, scope):
+
+    if is_monthly:
+        df = psmsl_df.loc[psmsl_df['location'].isin(locations)]
+    else:
+        df = psmsl_df_an.loc[psmsl_df_an['location'].isin(locations)]
+
+    df = df[df.year >= year]
+    fig = px.scatter(df, x="year", y="level", color="location", title=f'Annual Mean Tide, Overall Trend Since {year}',
+               trendline=trendline, trendline_scope=scope, color_discrete_map=data.colors)
+    if mode == 'lines':
+        fig.update_traces(mode=mode)
+        fig.data[len(locations)].update(mode='lines')
+    fig.update_layout(xaxis_title="Year", yaxis_title="Tide Level above RLR in mm", legend_title="Location")
+    return fig
+
+
+# @app.callback(
+#     Output('uhslc_daily', 'figure'),
+#     Input('uhslc_station', 'value')
+# )
+# def uhslc_daily(locations):
+#     fig = go.Figure()
+#     # https://plotly.com/python/reference/layout/
+#     fig.update_layout(title_text='Daily Tide Gauge', xaxis_title='Date', yaxis_title='Tide Level above Datum in mm', showlegend=True)
+#     fig.update_xaxes(showline=True, linewidth=.5, linecolor='black', mirror=True)
+#     fig.update_yaxes(showline=True, linewidth=.5, linecolor='black', mirror=True)
+#     if locations:
+#         for location in locations:
+#             df = uhslc_df.loc[uhslc_df['location'] == location]
+#             # https://plotly.com/python/reference/scatter/
+#             fig.add_trace(go.Scatter(x=df['date'], y=df['level'], mode='lines', name=location, marker_color=data.colors[location]))
+#     return fig
+
+
+@app.callback(
+    Output('location', 'figure'),
+    Input('station', 'value')
+)
+def station_location(locations):
+    fig = go.Figure(go.Scattergeo())
+
+    # https://plotly.com/python/reference/layout/geo/
+    fig.update_geos(lataxis_range=[32, 42], lonaxis_range=[-130, -112], resolution=50, scope='north america',
+                    subunitcolor="Black", subunitwidth=0.2)
+
+    # https://plotly.com/python/reference/layout/
+    fig.update_layout(title_text='Location of Tide Gauge Station')
+
+    if locations:
+        df = psmsl_df[psmsl_df['location'].isin(locations)]
+        df = df[['location', 'latitude', 'longitude']].drop_duplicates()
+        # https://plotly.com/python/reference/scattergeo/
+
+        for location in locations:
+            fig.add_trace(go.Scattergeo(
+            lon=df.loc[df['location'] == location, 'longitude'],
+            lat=df.loc[df['location'] == location, 'latitude'],
+            text=df.loc[df['location'] == location, 'location'],
+            textposition="middle left",
+            mode='markers+text',
+            marker_size=8,
+            showlegend=False,
+            name=location,
+            marker_color=data.colors[location],
+            hoverinfo="skip"
+            ))
+    return fig
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server()
---
+++ data.py
@@ -0,0 +1,108 @@
+from plotly.colors import qualitative
+
+use_remote = False
+
+colors = {
+    'Crescent City' : qualitative.Light24[0],
+    'Humboldt Bay'  : qualitative.Light24[1],
+    'Arena Cove'    : qualitative.Light24[2],
+    'Port Reyes'    : qualitative.Light24[3],
+    'San Francisco' : qualitative.Light24[4],
+    'Port Chicago'  : qualitative.Light24[5],
+    'Alameda'       : qualitative.Light24[6],
+    'Redwood City'  : qualitative.Light24[7],
+    'Monterey'      : qualitative.Light24[8],
+    'Port San Luis' : qualitative.Light24[9],
+    'Santa Barbara' : qualitative.Light24[10],
+    'Santa Monica'  : qualitative.Light24[11],
+    'Los Angeles'   : qualitative.Light24[12],
+    'La Jolla'      : qualitative.Light24[13],
+    'San Diego'     : qualitative.Light24[14]
+}
+
+# CO2 concentration data
+co2_data1 = "https://scrippsco2.ucsd.edu/assets/data/atmospheric/stations/in_situ_co2/monthly/monthly_in_situ_co2_mlo.csv" if use_remote else "data/monthly_in_situ_co2_mlo.csv"
+co2_data2 = "https://gml.noaa.gov/webdata/ccgg/trends/co2/co2_mm_gl.csv" if use_remote else "data/co2_mm_gl.csv"
+
+# UHSLC daily tide gauge research quality data
+uhslc_cresc = "https://uhslc.soest.hawaii.edu/data/csv/rqds/pacific/daily/d556a.csv" if use_remote else "data/d556a.csv"
+uhslc_humbo = "https://uhslc.soest.hawaii.edu/data/csv/rqds/pacific/daily/d576a.csv" if use_remote else "data/d576a.csv"
+uhslc_arena = "https://uhslc.soest.hawaii.edu/data/csv/rqds/pacific/daily/d573a.csv" if use_remote else "data/d573a.csv"
+uhslc_sanfr = "https://uhslc.soest.hawaii.edu/data/csv/rqds/pacific/daily/d551a.csv" if use_remote else "data/d551a.csv"
+uhslc_monte = "https://uhslc.soest.hawaii.edu/data/csv/rqds/pacific/daily/d555a.csv" if use_remote else "data/d555a.csv"
+uhslc_ports = "https://uhslc.soest.hawaii.edu/data/csv/rqds/pacific/daily/d565a.csv" if use_remote else "data/d565a.csv"
+uhslc_losan = "https://uhslc.soest.hawaii.edu/data/csv/rqds/pacific/daily/d567a.csv" if use_remote else "data/d567a.csv"
+uhslc_santm = "https://uhslc.soest.hawaii.edu/data/csv/rqds/pacific/daily/d578a.csv" if use_remote else "data/d578a.csv"
+uhslc_santb = "https://uhslc.soest.hawaii.edu/data/csv/rqds/pacific/daily/d577a.csv" if use_remote else "data/d577a.csv"
+uhslc_lajol = "https://uhslc.soest.hawaii.edu/data/csv/rqds/pacific/daily/d554a.csv" if use_remote else "data/d554a.csv"
+uhslc_sandi = "https://uhslc.soest.hawaii.edu/data/csv/rqds/pacific/daily/d569a.csv" if use_remote else "data/d569a.csv"
+uhslc_oilpl = "https://uhslc.soest.hawaii.edu/data/csv/rqds/pacific/daily/d594a.csv" if use_remote else "data/d594a.csv"
+
+uhslc_list = [
+    # id, location,         daily,          [latitude, longitude]
+    [556, 'Crescent City',  uhslc_cresc,    [41.74500, -124.18300]],
+    [576, 'Humboldt Bay',   uhslc_humbo,    [40.76700, -124.21700]],
+    [573, 'Arena Cove',     uhslc_arena,    [38.91300, -123.70800]],
+    [551, 'San Francisco',  uhslc_sanfr,    [37.80700, -122.46500]],
+    [555, 'Monterey',       uhslc_monte,    [36.60500, -121.88800]],
+    [565, 'Port San Luis',  uhslc_ports,    [35.17700, -120.76000]],
+#   [594, 'Oil Platform',   uhslc_oilpl,    [34.46800, -120.67300]],
+    [577, 'Santa Barbara',  uhslc_santb,    [34.40800, -119.68500]],
+    [578, 'Santa Monica',   uhslc_santm,    [34.00800, -118.50000]],
+    [567, 'Los Angeles',    uhslc_losan,    [33.72000, -118.27200]],
+    [554, 'La Jolla',       uhslc_lajol,    [32.86700, -117.25700]],
+    [569, 'San Diego',      uhslc_sandi,    [32.71300, -117.17300]]
+]
+
+# PSMSL monthly tide gauge research quality data
+psmsl_cresc_mon = "https://psmsl.org/data/obtaining/rlr.monthly.data/378.rlrdata" if use_remote else "data/378.rlrdata"
+psmsl_humbo_mon = "https://psmsl.org/data/obtaining/rlr.monthly.data/1639.rlrdata" if use_remote else "data/1639.rlrdata"
+psmsl_arena_mon = "https://psmsl.org/data/obtaining/rlr.monthly.data/2125.rlrdata" if use_remote else "data/2125.rlrdata"
+psmsl_reyes_mon = "https://psmsl.org/data/obtaining/rlr.monthly.data/1394.rlrdata" if use_remote else "data/1394.rlrdata"
+psmsl_sanfr_mon = "https://psmsl.org/data/obtaining/rlr.monthly.data/10.rlrdata" if use_remote else "data/10.rlrdata"
+psmsl_portc_mon = "https://psmsl.org/data/obtaining/rlr.monthly.data/2330.rlrdata" if use_remote else "data/2330.rlrdata"
+psmsl_alame_mon = "https://psmsl.org/data/obtaining/rlr.monthly.data/437.rlrdata" if use_remote else "data/437.rlrdata"
+psmsl_redwo_mon = "https://psmsl.org/data/obtaining/rlr.monthly.data/2329.rlrdata" if use_remote else "data/2329.rlrdata"
+psmsl_monte_mon = "https://psmsl.org/data/obtaining/rlr.monthly.data/1352.rlrdata" if use_remote else "data/1352.rlrdata"
+psmsl_ports_mon = "https://psmsl.org/data/obtaining/rlr.monthly.data/508.rlrdata" if use_remote else "data/508.rlrdata"
+psmsl_santb_mon = "https://psmsl.org/data/obtaining/rlr.monthly.data/2126.rlrdata" if use_remote else "data/2126.rlrdata"
+psmsl_santm_mon = "https://psmsl.org/data/obtaining/rlr.monthly.data/377.rlrdata" if use_remote else "data/377.rlrdata"
+psmsl_losan_mon = "https://psmsl.org/data/obtaining/rlr.monthly.data/245.rlrdata" if use_remote else "data/245.rlrdata"
+psmsl_lajol_mon = "https://psmsl.org/data/obtaining/rlr.monthly.data/256.rlrdata" if use_remote else "data/256.rlrdata"
+psmsl_sandi_mon = "https://psmsl.org/data/obtaining/rlr.monthly.data/158.rlrdata" if use_remote else "data/158.rlrdata"
+
+# PSMSL annual tide gauge research quality data
+psmsl_cresc_ann = "https://psmsl.org/data/obtaining/rlr.annual.data/378.rlrdata" if use_remote else "data/378a.rlrdata"
+psmsl_humbo_ann = "https://psmsl.org/data/obtaining/rlr.annual.data/1639.rlrdata" if use_remote else "data/1639a.rlrdata"
+psmsl_arena_ann = "https://psmsl.org/data/obtaining/rlr.annual.data/2125.rlrdata" if use_remote else "data/2125a.rlrdata"
+psmsl_reyes_ann = "https://psmsl.org/data/obtaining/rlr.annual.data/1394.rlrdata" if use_remote else "data/1394a.rlrdata"
+psmsl_sanfr_ann = "https://psmsl.org/data/obtaining/rlr.annual.data/10.rlrdata" if use_remote else "data/10a.rlrdata"
+psmsl_portc_ann = "https://psmsl.org/data/obtaining/rlr.annual.data/2330.rlrdata" if use_remote else "data/2330a.rlrdata"
+psmsl_alame_ann = "https://psmsl.org/data/obtaining/rlr.annual.data/437.rlrdata" if use_remote else "data/437a.rlrdata"
+psmsl_redwo_ann = "https://psmsl.org/data/obtaining/rlr.annual.data/2329.rlrdata" if use_remote else "data/2329a.rlrdata"
+psmsl_monte_ann = "https://psmsl.org/data/obtaining/rlr.annual.data/1352.rlrdata" if use_remote else "data/1352a.rlrdata"
+psmsl_ports_ann = "https://psmsl.org/data/obtaining/rlr.annual.data/508.rlrdata" if use_remote else "data/508a.rlrdata"
+psmsl_santb_ann = "https://psmsl.org/data/obtaining/rlr.annual.data/2126.rlrdata" if use_remote else "data/2126a.rlrdata"
+psmsl_santm_ann = "https://psmsl.org/data/obtaining/rlr.annual.data/377.rlrdata" if use_remote else "data/377a.rlrdata"
+psmsl_losan_ann = "https://psmsl.org/data/obtaining/rlr.annual.data/245.rlrdata" if use_remote else "data/245a.rlrdata"
+psmsl_lajol_ann = "https://psmsl.org/data/obtaining/rlr.annual.data/256.rlrdata" if use_remote else "data/256a.rlrdata"
+psmsl_sandi_ann = "https://psmsl.org/data/obtaining/rlr.annual.data/158.rlrdata" if use_remote else "data/158a.rlrdata"
+
+psmsl_list = [
+    # id,   location,         monthly,          annual,           [latitude, longitude]
+    [378,   'Crescent City',  psmsl_cresc_mon,  psmsl_cresc_ann,  [41.745, -124.181667]],
+    [1639,  'Humboldt Bay',   psmsl_humbo_mon,  psmsl_humbo_ann,  [40.766667, -124.216667]],
+    [2125,  'Arena Cove',     psmsl_arena_mon,  psmsl_arena_ann,  [38.913333, -123.706667]],
+    [1394,  'Port Reyes',     psmsl_reyes_mon,  psmsl_reyes_ann,  [37.995, -122.976667]],
+    [10,    'San Francisco',  psmsl_sanfr_mon,  psmsl_sanfr_ann,  [37.806667, -122.465]],
+    [2330,  'Port Chicago',   psmsl_portc_mon,  psmsl_portc_ann,  [38.055, -122.04]],
+    [437,   'Alameda',        psmsl_alame_mon,  psmsl_alame_ann,  [37.771667, -122.298333]],
+    [2329,  'Redwood City',   psmsl_redwo_mon,  psmsl_redwo_ann,  [37.506667, -122.211667]],
+    [508,   'Monterey',       psmsl_monte_mon,  psmsl_monte_ann,  [36.605, -121.886667]],
+    [2126,  'Port San Luis',  psmsl_ports_mon,  psmsl_ports_ann,  [35.176667, -120.76]],
+    [377,   'Santa Barbara',  psmsl_santb_mon,  psmsl_santb_ann,  [34.408333, -119.685]],
+    [377,   'Santa Monica',   psmsl_santm_mon,  psmsl_santm_ann,  [34.008333, -118.5]],
+    [245,   'Los Angeles',    psmsl_losan_mon,  psmsl_losan_ann,  [33.72, -118.271667]],
+    [256,   'La Jolla',       psmsl_lajol_mon,  psmsl_lajol_ann,  [32.866667, -117.256667]],
+    [158,   'San Diego',      psmsl_sandi_mon,  psmsl_sandi_ann,  [32.713333, -117.173333]]
+]
