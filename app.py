6090596490cd92b6d7d40af78fdd0e1a9c985f33
Mark <mcallinder@users.noreply.github.com>
2023-05-22 11:09:56 -0700

    Rolled back config file rename.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import dash_bootstrap_components as dbc
 
 app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-# Gunicorn server, see config.py
+# Gunicorn server, see gunicorn.conf.py
 server = app.server
 
 # Set up navigation bar and main container for all pages
