7cac85855eb4be01fc579fe82357fad57a1bbff0
Mark <mcallinder@users.noreply.github.com>
2023-05-22 11:26:23 -0700

    Formatting update.

--- app.py
+++ app.py
@@ -8,49 +8,27 @@ app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTSTRAP]
 server = app.server
 
 # Set up navigation bar and main container for all pages
-app.layout = html.Div(
-    [
-        dbc.NavbarSimple(
-            children=[
-                html.Div(
-                    [
-                        html.Div(
-                            [
-                                dcc.Link(
-                                    f"{page['name']}",
-                                    href=page["relative_path"],
-                                    style={'textDecoration': 'none', 'color': 'white'}
-                                )
-                            ],
-                            style={'paddingLeft': '20px', 'display': 'inline-block'}
-                        ) for page in page_registry.values()
-                    ]
-                ),
-            ],
-            brand=title,
-            brand_href='/',
-            color='dark',
-            dark=True,
-        ),
-        dbc.Row(
-            [
-                dbc.Col(
-                    md=1,
-                    lg=1
-                ),
-                dbc.Col(
-                    [
-                        html.Br(),
-                        page_container
-                    ],
-                    md=10,
-                    lg=10
+app.layout = html.Div([
+    dbc.NavbarSimple([
+        html.Div([
+            html.Div([
+                dcc.Link(
+                    f"{page['name']}",
+                    href=page["relative_path"],
+                    style={'textDecoration': 'none', 'color': 'white'}
                 )
-            ]
-        ),
-        html.Br()
-    ]
-)
+            ], style={'paddingLeft': '20px', 'display': 'inline-block'}) for page in page_registry.values()
+        ]),
+    ], brand=title, brand_href='/', color='dark', dark=True),
+    dbc.Row([
+        dbc.Col(md=1, lg=1),
+        dbc.Col([
+            html.Br(),
+            page_container
+        ], md=10, lg=10)
+    ]),
+    html.Br()
+])
 
 
 if __name__ == '__main__':
